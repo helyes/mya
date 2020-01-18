c123b4a78d1c5532be9c1c6a3bb5e8fde5a88c18
helyes <helyesa@gmail.com>
2020-01-18 13:44:40 +1000

    Convert try 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+use serde::{Serialize, Deserialize};
+use std::collections::BTreeMap;
+
+// #[derive(Debug, PartialEq, Serialize, Deserialize)]
+// struct Application {
+//     application: Environment,
+// }
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Command {
+    command: String,
 }
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum Environment {
+    Hash(BTreeMap<String, Command>),
+}
+
+fn main() {
+
+  let content_hash = r#"
+  {
+        "ONE_ENV": 
+          {command : "fake" }
+  }
+  "#;
+  let application_data: Environment = serde_yaml::from_str(content_hash).unwrap();
+  println!("{:?}", application_data);
+  
+
+  match serde_yaml::to_string(&application_data) {
+      Ok(s1) => println!("{:?}", s1), 
+      Err(_) => { println!("Erro") },
+  };
+  
+
+  let file_to_read = get_snipet_file_path();
+  println!("Reading file: {}", file_to_read);
+
+  let contents = fs::read_to_string(file_to_read)
+    .expect("Something went wrong reading the file");
+
+  println!("With text:\n{}", contents);
+
+}
+
+fn get_snipet_file_path() -> String {
+  let file_path = String::from("snippets/shell.yaml"); // some_string comes into scope
+  file_path                              
+}
+
+// fn parse_config(args: &[String]) -> (&str, &str) {
+//   let par1 = &args[1];
+//   let par2 = &args[2];
+
+//   (par1, par2)
+// }
