20dba582a953fbb32f723325f00438a73bc9262f
helyes <helyesa@gmail.com>
2020-01-18 14:11:18 +1000

    Loading yaml

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,40 @@ use std::fs;
 use serde::{Serialize, Deserialize};
 use std::collections::BTreeMap;
 
-// #[derive(Debug, PartialEq, Serialize, Deserialize)]
-// struct Application {
-//     application: Environment,
-// }
-
+// https://serde.rs/derive.html
+// https://github.com/dtolnay/serde-yaml
+// https://stackoverflow.com/questions/55245914/how-to-use-serde-to-parse-a-yaml-file-with-multiple-different-types
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Command {
+struct Details {
     command: String,
+    description: String,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
-enum Environment {
-    Hash(BTreeMap<String, Command>),
+enum Snippet {
+    Hash(BTreeMap<String, Details>),
 }
 
 fn main() {
 
-  let content_hash = r#"
-  {
-        "ONE_ENV": 
-          {command : "fake" }
-  }
-  "#;
-  let application_data: Environment = serde_yaml::from_str(content_hash).unwrap();
-  println!("{:?}", application_data);
-  
+  let args: Vec<String> = env::args().collect();
 
-  match serde_yaml::to_string(&application_data) {
-      Ok(s1) => println!("{:?}", s1), 
-      Err(_) => { println!("Erro") },
-  };
-  
+  let (action, key) = parse_config(&args);
+  println!("Action {}, key: {}", action, key);
 
   let file_to_read = get_snipet_file_path();
   println!("Reading file: {}", file_to_read);
 
   let contents = fs::read_to_string(file_to_read)
     .expect("Something went wrong reading the file");
+  let snippets: Snippet = serde_yaml::from_str(&contents).unwrap();
+  println!("{:?}", snippets);
 
-  println!("With text:\n{}", contents);
+  // let snippet_to_execute = snippets.get(&team_name);
+  
+
+  // println!("With text:\n{}", contents);
 
 }
 
@@ -53,9 +46,24 @@ fn get_snipet_file_path() -> String {
   file_path                              
 }
 
-// fn parse_config(args: &[String]) -> (&str, &str) {
-//   let par1 = &args[1];
-//   let par2 = &args[2];
+fn parse_config(args: &[String]) -> (&str, &str) {
+  let action = &args[1];
+  let key = &args[2];
 
-//   (par1, par2)
-// }
+  (action, key)
+}
+
+  // let content_hash = r#"
+  // {
+  //       "ONE_ENV": 
+  //         {command : "fake" }
+  // }
+  // "#;
+  // let snippet_data: Snippet = serde_yaml::from_str(content_hash).unwrap();
+  // println!("{:?}", snippet_data);
+  
+
+  // match serde_yaml::to_string(&snippet_data) {
+  //     Ok(s1) => println!("TOSTRING{:?}", s1), 
+  //     Err(_) => { println!("Error") },
+  // };
