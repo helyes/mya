14152a73193c06d4f8c04b61a6c3eb7134391376
helyes <helyesa@gmail.com>
2020-01-19 12:56:48 +1000

    Added list action to print available snippets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // https://doc.rust-lang.org/rust-by-example/custom_types/enum/testcase_linked_list.html
 // https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html
-use std::collections::HashMap;
+use colored::*;
+use std::collections::BTreeMap;
 use std::env;
 use std::fs;
 use std::process::Command;
@@ -23,66 +24,124 @@ struct Details {
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
 enum Snippet {
-    Commands(HashMap<String, Details>),
+    Commands(BTreeMap<String, Details>),
 }
 
+#[derive(Debug)]
 enum Action {
-  Run,
-  List
+  Run(String),
+  Help,
+  List(ActionListMode),
+  Unknown
 }
 
-fn list() {
-  println!("LIST");
+#[derive(Debug)]
+enum ActionListMode {
+  Short, // this is for bash code completition
+  Verbose
 }
 
-fn run() {
-  println!("RUN");
-}
-
-struct Execute {
-  action: Action,
-  arguments: String,
-}
+// #[derive(Debug)]
+// struct Executable {
+//   action: Action,
+//   key: String,
+// }
 
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+fn parse_config(args: &[String]) -> Action {
   println!("Parsing args {:?}", args);
+  let mut ret: Action = Action::Unknown;
   match args {
     [only_one] => { 
       println!("help for {}", only_one);
-      process::exit(0);
+      ret = Action::Help;
     },
-    [_first, action] => println!(
-        "Only action given: {}",
-        action
-    ),
+    [_first, action] => {
+      match action.as_str() {
+        "help" => ret = Action::Help,
+        "list" => ret = Action::List(ActionListMode::Verbose),
+        _ => println!("Wrong parameter")
+      }
+
+    },
     [_first, action, param] => { 
       println!("Params: {} and {}",action, param);
-      return (action, param)
+      match action.as_str() {
+        "run" => ret = Action::Run(String::from(param)),
+        "list" => ret = Action::List(ActionListMode::Short), // whatever comes after list will result in short list
+        _ => println!("NOT a Run or List action")
+      }
+      // println!("!!!!!!!!!Acrion enum: {:?}", &ret);
     },
     _ => println!("Too many parameters: {}", args.len()),
   }
 
-  ("action", "key")
+  return ret;
 }
 
-
-fn main() {
-
-  let args: Vec<String> = env::args().collect();
-
-  let (action, key) = parse_config(&args);
-  println!("Action {}, key: {}", action, key);
-
+fn read_snippets() -> Snippet {
   let file_to_read = get_snipet_file_path();
   println!("Reading file: {}", file_to_read);
 
   let contents = fs::read_to_string(file_to_read)
     .expect("Something went wrong reading the file");
   let snippets: Snippet = serde_yaml::from_str(&contents).unwrap();
-  // println!("{:?}", snippets);
+  return snippets;
+}
 
-  let available_snippets: HashMap<String, Details>;
+
+fn left_pad(s: &str, pad: usize, padchar: char) -> String
+{
+    let mut ret = String::new();
+    for _ in 0..pad {
+        ret.push(padchar);
+    }
+    ret.push_str(s);
+    ret
+}
+
+fn list(mode: ActionListMode) {
+  println!("LIST");
+  let snippets: Snippet = read_snippets();
+  println!("Snippets: {:?}", snippets);
+  let available_snippets: BTreeMap<String, Details>;
+  match snippets {
+    Snippet::Commands(value) => {
+      // println!("value: {:?}", value);
+      available_snippets = value;
+    }
+  }
+  //println!("Available Snippets: {:?}", available_snippets);
+
+  match mode {
+    ActionListMode::Short => {
+      for (key, _value) in &available_snippets {
+        print!("{} ", key);
+      }
+    },
+    ActionListMode::Verbose => {
+      println!("\n{}", "Available commands".green().bold());
+      let mut longest_key_length = 0;
+      for (key, _value) in &available_snippets {
+        if key.len() > longest_key_length {
+          longest_key_length = key.len();
+        }
+      }
+      for (key, value) in &available_snippets {
+        let keyp = format!("  {}", key);
+        println!("{} {}", keyp.yellow().bold(), left_pad(&value.command, longest_key_length - key.len(), ' ').bold());
+        println!("{}", left_pad(&value.description, longest_key_length + 3, ' '));
+      }
+    }
+  }
+}
+
+
+fn run(key: &str) {
+  println!("RUNNING {}", &key);
+
+  let snippets: Snippet = read_snippets();
+  let available_snippets: BTreeMap<String, Details>;
   match snippets {
     Snippet::Commands(value) => {
       // println!("value: {:?}", value);
@@ -123,6 +182,29 @@ fn main() {
         process::exit(255)
       }
     }
+}
+
+fn main() {
+
+  let args: Vec<String> = env::args().collect();
+  let a = parse_config(&args);
+  println!("AAAAA:{:?}", a);
+
+  match a {
+    Action::Help =>  {
+      println!("Printing help");
+      process::exit(0);
+    },
+    Action::List(l) =>  {
+      list(l);
+    }
+    Action::Run(key) =>  {
+      run(&key);
+    }
+    _ => {
+      println!("Action not implemented {:?}", a);
+    }
+  };
 
 }
 
@@ -187,11 +269,4 @@ fn get_command_without_parameters (command: &String) -> String {
 fn get_snipet_file_path() -> String {
   let file_path = String::from("snippets/shell.yaml"); // some_string comes into scope
   file_path                              
-}
-
-fn parse_config_old(args: &[String]) -> (&str, &str) {
-  let action = &args[1];
-  let key = &args[2];
-
-  (action, key)
 }
